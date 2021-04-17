7e47a0d26667461768705aabcb7a8fc341dd6b83
unknown <bittugautam1224@gmail.com>
2021-04-17 15:07:32 +0530

    Second Commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,11 @@
 import firebase from "firebase";
 const firebaseConfig = {
-    apiKey: "AIzaSyCrcTYxvN9sBgKiqsFu0CBg8ry1aFH6AHo",
-    authDomain: "tinder-clone-50874.firebaseapp.com",
-    projectId: "tinder-clone-50874",
-    storageBucket: "tinder-clone-50874.appspot.com",
-    messagingSenderId: "367058050284",
-    appId: "1:367058050284:web:5f9a22594f247e4eea19c9"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
   const firebaseApp=firebase.initializeApp(firebaseConfig);
